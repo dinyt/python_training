d572cd3d7619a067c9828ed8b9f0de673b75c1d7
dinyt <angel_of_life@mail.ru>
2021-03-23 22:07:34 +0300

    Выделен класс для работы с контактами

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 class Application:
 
@@ -9,65 +10,11 @@ class Application:
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def destroy(self):
         self.wd.quit()
 
-    # =====================
-    # For groups
-    # =====================
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    # =====================
-    # For contacts
-    # =====================
-    def return_to_home_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home").click()
-
-    def create_contact(self, contact):
-        wd = self.wd
-        # click add contact
-        wd.find_element_by_link_text("add new").click()
-        # fill form fields - contact atributes
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.fName)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.mName)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lName)
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nick)
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home_phone)
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work_phone)
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.homePage)
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.phone2)
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
-        # save contact
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        self.return_to_home_page()
--- test/test_new_contact.py
+++ test/test_new_contact.py
@@ -11,14 +11,14 @@ def app(request):
 
 def test_new_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact("Isaak", "Ivanovich", "Nosov", "pes-barbos", "title for barbos", "something company",
+    app.contact.create_contact(Contact("Isaak", "Ivanovich", "Nosov", "pes-barbos", "title for barbos", "something company",
                         "Moscow, red square, 1", "89991234567", "none1", "none2", "none3", "barbos@mail.ru", "none4",
                         "none5", "http://barbos.page.org", "none6", "This is contact for barbos", "none7"))
     app.session.logout()
 
 def test_new_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact("", "", "", "", "", "",
+    app.contact.create_contact(Contact("", "", "", "", "", "",
                         "", "", "", "", "", "", "",
                         "", "", "", "", ""))
     app.session.logout()
