f1e19cc1cd2f9a6355206a6b60fd594a273625b1
dinyt <angel_of_life@mail.ru>
2021-04-03 12:45:06 +0300

    валидацию фикстуры перед каждым тестом и доработка вспомогательного класса для операций login и logout

--- fixture/application.py
+++ fixture/application.py
@@ -7,10 +7,18 @@ class Application:
 
     def __init__(self):
         self.wd = WebDriver()
+        # implicitly_wait - ожидание тех или иных элементов в браузере
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
 
     def destroy(self):
         self.wd.quit()
