3930dfe7b05964313e366f200affc28542d795c0
dinyt <angel_of_life@mail.ru>
2021-04-18 14:25:57 +0300

    Реализация загрузки информации о тестируемом приложении из конфигурационного файла

--- conftest.py
+++ conftest.py
@@ -1,17 +1,28 @@
 from fixture.application import Application
 import pytest
+import json
+import os.path
 
 fixture = None
+target = None
 
 @pytest.fixture(scope="session")
 def app(request):
     global fixture
-    if fixture is None:
-        fixture = Application()
-    else:
-        if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+    global target
+
+    browser = request.config.getoption("--browser")
+
+    if target is None:
+        config_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                        request.config.getoption("--target"))
+        with open(config_file_path) as config_file:
+            target = json.load(config_file)
+
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target["baseUrl"])
+
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 # scope - выполнение тестов в одной сессии
@@ -23,3 +34,7 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--target", action="store", default="target.json")
