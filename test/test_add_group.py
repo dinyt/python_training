e58acae94749943d258f013b8ee28d637b0199da
dinyt <angel_of_life@mail.ru>
2021-05-18 14:05:29 +0300

    Задание №29. Генерация отчётов при помощи Allure

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-#import pytest
+import pytest
 #from data.groups import constant as testdata
 
 #@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, db, json_groups):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-   # assert len(old_groups) + 1 == app.group.count()
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    # название шага такое же как и в файле bdd/groups.feature
+    with pytest.allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with pytest.allure.step('When I add the group %s to the list' % group):
+        app.group.create(group)
+    with pytest.allure.step('Then the new group list is equal to the old list with the added group'):
+       # assert len(old_groups) + 1 == app.group.count()
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
